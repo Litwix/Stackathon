cef164aead8bf89cd09666657ee21c89a8851ca4
Anshu Patel <anshupatel314@gmail.com>
2021-12-14 16:23:45 -0500

    moved rogue files under characters folder

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -9,7 +9,11 @@ export default class Preloader extends Phaser.Scene {
     this.load.image('dungeons', 'dungeons/DungeonTiles.png');
     this.load.tilemapTiledJSON('dungeon01', 'dungeons/dungeon-01.json');
 
-    this.load.atlas('rogue', 'character/Rogue.png', 'character/Rogue.json');
+    this.load.atlas(
+      'rogue',
+      'characters/rogue/Rogue.png',
+      'characters/rogue/Rogue.json'
+    );
     this.load.atlas(
       'slime',
       'characters/slime/Slime.png',
